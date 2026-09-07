a084c5bc37361c0e0f9b9479f07e1aa90b694ec4
Dominik Moritz <github@domoritz.de>
2026-09-06 22:20:10 -0400

    fix: refactor constant, improve circle handling, cleaner override, support negative

--- packages/vega-scenegraph/src/marks/symbol.js
+++ packages/vega-scenegraph/src/marks/symbol.js
@@ -1,7 +1,7 @@
 import Bounds from '../Bounds.js';
 import boundContext from '../bound/boundContext.js';
 import boundStroke from '../bound/boundStroke.js';
-import {symbol} from '../path/shapes.js';
+import {DefaultSymbolSize, symbol} from '../path/shapes.js';
 import {intersectPoint} from '../util/intersect.js';
 import markItemPath from './markItemPath.js';
 
@@ -10,15 +10,12 @@ const unitBounds = Object.create(null);
 /** Size used to construct unit bounds, resulting in a scale factor of 1. */
 const UNIT_SYMBOL_SIZE = 4;
 
-/** Fallback symbol size used when no size is specified. Results in radius of 4. */
-const FALLBACK_SYMBOL_SIZE = 64;
-
 function unitBox(shape) {
   let box = unitBounds[shape];
 
   if (box === undefined) {
     const b = new Bounds();
-    symbol(boundContext(b, 0), { shape, size: UNIT_SYMBOL_SIZE });
+    symbol(boundContext(b, 0), {shape, size: UNIT_SYMBOL_SIZE});
     box = unitBounds[shape] = [b.x1, b.y1, b.x2, b.y2];
   }
 
@@ -26,21 +23,22 @@ function unitBox(shape) {
 }
 
 function bound(bounds, item) {
-  if (item.angle) {
-    // Rotated shapes need their bounds recalculated.
+  const shape = item.shape || 'circle';
+
+  if (item.angle && shape !== 'circle') {
     symbol(boundContext(bounds, item.angle), item);
   } else {
-    const u = unitBox(item.shape || 'circle');
-    const size = item.size ?? FALLBACK_SYMBOL_SIZE;
-    const r = Math.sqrt(size) / 2;
+    const u = unitBox(shape);
+    const r = Math.sqrt(item.size ?? DefaultSymbolSize) / 2;
 
-    bounds.set(u[0] * r, u[1] * r, u[2] * r, u[3] * r);
+    if (r >= 0) {
+      bounds.set(u[0] * r, u[1] * r, u[2] * r, u[3] * r);
+    } else {
+      bounds.clear();
+    }
   }
 
   return boundStroke(bounds, item, true).translate(item.x || 0, item.y || 0);
 }
 
-export default {
-  ...markItemPath('symbol', symbol, intersectPoint),
-  bound
-};
+export default markItemPath('symbol', symbol, intersectPoint, bound);
